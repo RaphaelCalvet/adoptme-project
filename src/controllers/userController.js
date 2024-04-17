4d8313c3cb2acaace3be12b3d2aa68bb8312e46f
raphael <raphael.calvet@dmslog.com>
2024-04-17 18:54:47 -0300

    criando classe  userController

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,30 @@
 const User = require('../models/userModel');
+const {all} = require("express/lib/application");
+
+class UserController {
+
+    async getAllUsers(req, res) {
+        try {
+            const users = await User.find(undefined, undefined, undefined);
+            res.status(200).json(users);
+        }
+        catch(error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+    async getUserById(req, res) {
+        const { id } = req.params;
+        try {
+            const user = await User.findById(id);
+            if (!user) {
+                return res.status(404).json({message: `User with id ${id} not found`});
+            }
+        }
+        catch(error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+    async createUser(req, res) {}
+}
